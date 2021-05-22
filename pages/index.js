ae4799364c33931150f0e68f50798a4d1efb8620
Skubko <sam1177@mail.ru>
2021-05-23 04:49:16 +0700

    sprint-4-1.01

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector ('.input__text-name'); 
+let jobInput = formElement.querySelector ('.input__text-job');
+let Firstname = document.querySelector ('.forma__name');
+let profession = document.querySelector ('.forma__profession');
+let popup = document.querySelector ('.popup');
+let EditButton = document.querySelector('.Profile-Info__EditButton');
+let SubmitButton = document.querySelector('.SubmitButton');
+let CloseIcon = document.querySelector('.Close-Icon');
+
+
+function formSubmitHandler (evt) {
+evt.preventDefault();
+Firstname.textContent = nameInput.value;
+profession.textContent = jobInput.value;
+popup.classList.toggle('popup_opened');
+}
+
+function formCloseHandler () {
+popup.classList.toggle('popup_opened');
+}
+
+function PopupOpened () {
+popup.classList.add('popup_opened');
+}
+
+EditButton.addEventListener('click', PopupOpened); 
+SubmitButton.addEventListener('click', formSubmitHandler);
+CloseIcon.addEventListener('click', formCloseHandler); 
